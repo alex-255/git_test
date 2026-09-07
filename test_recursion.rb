e9ae0664e1afce03c1bf8f75581397abc6b7cd3d
Oleksandr Kozlov <wpok13@gmail.com>
2026-09-07 17:53:26 +0200

    Refactor according to the Rubocop

--- test_recursion.rb
+++ test_recursion.rb
@@ -1,34 +1,34 @@
-arr = [1, 2, 3, [5,6,[8,9,10],7], 11, [12, 13], 14, 15]
+arr = [1, 2, 3, [5, 6, [8, 9, 10], 7], 11, [12, 13], 14, 15]
 
 def recursion_trough_arr(current_arr, index = 0, level = 0)
   # if last element in a row
   if index >= current_arr.length - 1
 
     if current_arr[index].is_a?(Array)
-      puts print_dashes(level) + "--" + unicode_symbol("\u25BD") # symbol "corner" for one level deeper
+      puts "#{print_dashes(level)}--#{unicode_symbol("\u25BD")}" # symbol "corner" for one level deeper
       recursion_trough_arr(current_arr[index], 0, level + 1)
     else
-      puts print_dashes(level) + " " + current_arr[index].to_s
+      puts "#{print_dashes(level)} #{current_arr[index]}"
     end
 
   else
 
     # if not last element in a row
     if current_arr[index].is_a?(Array)
-      puts print_dashes(level) + "--" + unicode_symbol("\u25BD") # symbol "corner" for one level deep
+      puts "#{print_dashes(level)} #{unicode_symbol("\u25BD")}" # symbol "corner" for one level deep
       recursion_trough_arr(current_arr[index], 0, level + 1)
       recursion_trough_arr(current_arr, index + 1, level) # calling recursion second time that it continue working till the end of row
     else
-      puts print_dashes(level) + " " + current_arr[index].to_s
+      puts "#{print_dashes(level)} #{current_arr[index]}"
       recursion_trough_arr(current_arr, index + 1, level)
     end
 
   end
 end
 
-def print_dashes(level) 
-  dashes = "-"
-  level.times { dashes += "-" }
+def print_dashes(level)
+  dashes = '-'
+  level.times { dashes += '-' }
   dashes
 end
 
@@ -37,6 +37,3 @@ def unicode_symbol(symbol)
 end
 
 recursion_trough_arr(arr)
-
-
-  
